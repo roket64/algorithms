41d4c4dc620662f5ba9c03ea1dfd7201eef4df10
roket64 <1p41i4y1@gmail.com>
2023-02-04 17:50:48 +0900

    gcd impl

--- number_theory/euclidean.py
+++ number_theory/euclidean.py
@@ -1,2 +1,28 @@
-def gcd(n: int, m: int) -> int:
-    pass
+def gcd(x: int, y: int) -> int:
+    if (x == y):
+        return x
+    if (x == 0):
+        return y 
+    if (y == 0):
+        return x
+
+    k = 0
+    while (((x | y) & 1) == 0):
+        x >>= 1
+        y >>= 1
+        k += 1
+
+    while (~x & 1):
+        x >>= 1
+
+    while (y):
+
+        while (~y & 1):
+            y >>= 1
+
+        if (x > y):
+            x, y = y, x
+
+        y -= x
+
+    return (x << k)
