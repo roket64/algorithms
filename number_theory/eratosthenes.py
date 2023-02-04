ff0224339fa523e5a856a0088d481fed7651eea1
roket64 <1p41i4y1@gmail.com>
2023-02-04 19:38:47 +0900

    minor upgrade sieve

--- number_theory/eratosthenes.py
+++ number_theory/eratosthenes.py
@@ -2,6 +2,7 @@ from math import sqrt
 
 
 def naive_sieve(sz: int = 1000000) -> tuple:
+    phi = [0 for _ in range(sz + 1)]
     isp = [True for _ in range(sz + 1)]
     spf = [-1 for _ in range(sz + 1)]
 
@@ -11,7 +12,6 @@ def naive_sieve(sz: int = 1000000) -> tuple:
     for i in range(2, sqrtn + 1):
         if (isp[i]):
             spf[i] = i
-
             for j in range(i * i, sz + 1, i):
                 isp[j] = False
                 if (spf[j] == -1):
@@ -21,17 +21,31 @@ def naive_sieve(sz: int = 1000000) -> tuple:
 
 
 def linear_sieve(sz: int = 1000000) -> tuple:
-    spf = [0 for _ in range(sz + 1)]
-    ret = []
+    spf = [0 for _ in range(sz + 1)]  # spf[i]: smallest prime factor for i
+    phi = [0 for _ in range(sz + 1)]  # phi[i]: euler phi function for i
+    exp = [0 for _ in range(sz + 1)]  # exp[i]: exponent of smallest prime factor for i
+    mob = [0 for _ in range(sz + 1)]  # mob[i]: mobius function for i
+    ret = []  # primes in given interval [2, sz]
+
+    phi[1] = 1
+    mob[1] = 1
 
     for i in range(2, sz + 1):
         if (spf[i] == 0):
+            exp[i] = 1
+            phi[i] = i - 1
             spf[i] = i
+            mob[i] = -1
             ret.append(i)
 
         for j in ret:
-            if (i * j > sz): break
-            spf[i * j] = j 
-            if (i % j == 0): break
+            if (i * j > sz):
+                break
+            spf[i * j] = j
+            phi[i * j] = phi[i] * j
+            exp[i * j] = exp[i] + 1
+            mob[i * j] = 0
+            if (i % j == 0):
+                break
 
     return (spf, ret)
