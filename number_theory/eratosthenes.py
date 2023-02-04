8f30f0fea9873506554a9db65e30b7d2dc02f85a
roket64 <1p41i4y1@gmail.com>
2023-02-04 19:56:17 +0900

    minor fix sieve

--- number_theory/eratosthenes.py
+++ number_theory/eratosthenes.py
@@ -1,40 +1,23 @@
-from math import sqrt
-
-
-def naive_sieve(sz: int = 1000000) -> tuple:
-    phi = [0 for _ in range(sz + 1)]
-    isp = [True for _ in range(sz + 1)]
-    spf = [-1 for _ in range(sz + 1)]
-
-    isp[0] = isp[1] = False
-    sqrtn = int(sqrt(sz))
-
-    for i in range(2, sqrtn + 1):
-        if (isp[i]):
-            spf[i] = i
-            for j in range(i * i, sz + 1, i):
-                isp[j] = False
-                if (spf[j] == -1):
-                    spf[j] = i
-
-    return (isp, )
-
-
 def linear_sieve(sz: int = 1000000) -> tuple:
-    spf = [0 for _ in range(sz + 1)]  # spf[i]: smallest prime factor for i
-    phi = [0 for _ in range(sz + 1)]  # phi[i]: euler phi function for i
-    exp = [0 for _ in range(sz + 1)]  # exp[i]: exponent of smallest prime factor for i
-    mob = [0 for _ in range(sz + 1)]  # mob[i]: mobius function for i
-    ret = []  # primes in given interval [2, sz]
+    # spf[i]: smallest prime factor for i
+    # phi[i]: euler phi function for i
+    # exp[i]: exponent of smallest prime factor for i
+    # mob[i]: mobius function for i
+    # ret: primes in given interval [2, sz]
+    spf = [0 for _ in range(sz + 1)]
+    phi = [0 for _ in range(sz + 1)]
+    exp = [0 for _ in range(sz + 1)]
+    mob = [0 for _ in range(sz + 1)]
+    ret = []
 
     phi[1] = 1
     mob[1] = 1
 
     for i in range(2, sz + 1):
         if (spf[i] == 0):
-            exp[i] = 1
-            phi[i] = i - 1
             spf[i] = i
+            phi[i] = i - 1
+            exp[i] = 1
             mob[i] = -1
             ret.append(i)
 
@@ -48,4 +31,4 @@ def linear_sieve(sz: int = 1000000) -> tuple:
             if (i % j == 0):
                 break
 
-    return (spf, ret)
+    return (spf, phi, exp, mob, ret)
